e0e4c026b21ba1fd8d136933e819345e3fd9bf67
Sebastian Rey <areyprada@gmail.com>
2025-09-19 23:49:10 -0500

    se empieza proyecto

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Home from './componentes/Home.jsx'
+import Footer from './componentes/Footer.jsx'
+
+function App() {
+
+  return (
+    <>
+    <Home/>
+    <Footer/>
+    </>
+  )
+}
+
+export default App
---
+++ src/componentes/Home.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import video1 from "../../public/videoPagina1.mp4";
+import image1 from "../../public/image1.png";
+
+const Home = () => {
+  return (
+    <div className="home">
+      <div className="header-linea"></div>
+      <div className="header">
+        <video
+          src={video1}
+          controls
+          playsInline
+        />
+      </div>
+      <img className="header-image1" src={image1} alt="" />
+    </div>
+  );
+};
+
+export default Home;
